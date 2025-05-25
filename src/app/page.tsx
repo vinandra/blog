89eb068694475c27e5e4ccf5e4a06953c99a25ea
vinandra <vinandra207@gmail.com>
2025-05-25 22:40:33 +0700

    rewrite: nextjs14

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen">
+      <nav>
+        <div className="container mx-auto">
+          <div className="flex">
+            <div className="w-2/12">Blog</div>
+            <div className="w-6/12">nav</div>
+            <div className="w-4/12">search</div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/components/Create.js
+++
@@ -1,9 +0,0 @@
-const Create = () => {
-  return (
-    <div className="create">
-      <h2>add a new blog</h2>
-    </div>
-  );
-};
-
-export default Create;
